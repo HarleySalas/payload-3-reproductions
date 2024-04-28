db2202644011a32e2b29dbcef7168ad43be0c1e7
Harley <harley@harleysalas.com>
2024-04-29 02:06:15 +0300

    reproduction

--- src/app/payload/collections/page/Page.ts
+++ src/app/payload/collections/page/Page.ts
@@ -1,6 +1,37 @@
-import { CollectionConfig } from 'payload/types'
+import {
+  BlocksFeature,
+  FeatureProviderServer,
+  LexicalBlock,
+  lexicalEditor,
+} from '@payloadcms/richtext-lexical'
+import { Block, CollectionConfig } from 'payload/types'
 
 export const COLLECTION_SLUG_PAGE = 'page'
+
+export const LoginModule: Block = {
+  slug: 'login-module',
+  fields: [
+    {
+      name: 'subtitle',
+      type: 'richText',
+      localized: true,
+      editor: lexicalEditor(),
+    },
+  ],
+}
+
+export const Module: LexicalBlock = {
+  slug: 'module',
+  interfaceName: 'Module',
+  fields: [
+    {
+      name: 'module',
+      type: 'blocks',
+      maxRows: 1,
+      blocks: [LoginModule],
+    },
+  ],
+}
 
 export const Page: CollectionConfig = {
   slug: COLLECTION_SLUG_PAGE,
@@ -15,6 +46,12 @@ export const Page: CollectionConfig = {
     {
       name: 'content',
       type: 'richText',
+      editor: lexicalEditor({
+        features: ({ defaultFeatures }) => [
+          ...defaultFeatures,
+          BlocksFeature({ blocks: [Module] }) as FeatureProviderServer<unknown, unknown>,
+        ],
+      }),
     },
   ],
 }
