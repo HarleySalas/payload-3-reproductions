8623c4490c6c5fe937a0ca44ac559f979fdc3025
Harley <harley@harleysalas.com>
2024-05-12 12:29:19 +0300

    reproduction notes

--- src/payload/collections/tag/Tag.ts
+++ src/payload/collections/tag/Tag.ts
@@ -8,9 +8,30 @@ export const Tag: CollectionConfig = {
   admin: {
     useAsTitle: 'title',
   },
+  // // works in relationship! (boolean evaluation)
+  // access: {
+  // 	create: () => true,
+  // 	read: () => true,
+  // 	update: () => true,
+  // 	delete: () => true,
+  // },
+
+  /**
+   * Does not work in relationship!
+   * Assumingly because it uses a query Object as a returned value,
+   * as opposed to a boolean result.
+   */
   access: {
+    /**
+     * Can open create modal
+     * No save button
+     */
     create: (args) => access({ args, allowedRoles: ['user'] }), //users can create tags
     read: () => true, //anyone can read tags
+    /**
+     * Update partially works. User role cannot update the related document,
+     * however, they can open the update modal.
+     */
     update: (args) => access({ args }), //only admins can modify tags once created
     delete: (args) => access({ args }), //only admins can delete tags
   },
