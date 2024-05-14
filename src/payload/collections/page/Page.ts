599b41189ef43fdf9b51bb62f7b0f31c0b41234c
Harley <harley@harleysalas.com>
2024-05-14 20:16:03 +0300

    reproduction

--- src/payload/collections/page/Page.ts
+++ src/payload/collections/page/Page.ts
@@ -1,9 +1,13 @@
+import { revalidateTag } from 'next/cache'
 import { CollectionConfig } from 'payload/types'
 
 export const COLLECTION_SLUG_PAGE = 'page'
 
 export const Page: CollectionConfig = {
   slug: COLLECTION_SLUG_PAGE,
+  hooks: {
+    afterChange: [() => revalidateTag(COLLECTION_SLUG_PAGE)],
+  },
   admin: {
     useAsTitle: 'title',
   },
